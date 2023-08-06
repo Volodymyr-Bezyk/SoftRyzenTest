55e3195b155867104b2072f372a3a8c413965f0c
Volodymyr-Bezyk <99117313+Volodymyr-Bezyk@users.noreply.github.com>
2023-08-06 12:34:39 +0000

    Deploying to gh-pages from @ Volodymyr-Bezyk/softRyzenTest@5d367ade9f6c8cca77bcfbee21347ebfc9ee0a39 🚀

---
+++ static/js/47.64aa0726.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[47],{8769:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(7769),a=n(4453),c=n(5325),s=n(184),u=function(){return(0,s.jsxs)(s.Fragment,{children:[(0,s.jsx)(r.Z,{}),(0,s.jsx)(a.Z,{title:"Create new event",children:(0,s.jsx)(c.Z,{page:"newEvent"})})]})}}}]);
+//# sourceMappingURL=47.64aa0726.chunk.js.map
